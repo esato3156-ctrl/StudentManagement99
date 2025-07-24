70bb1e5120e763e907b2d52f2841bcd055d18b5b
Etsuko Sato <e.sato3156@gmail.com>
2025-07-24 19:54:03 +0900

    Update .gitignore and remove unwanted files from tracking

---
+++ src/test/java/risetech/studentmanagement/StudentManagement99ApplicationTests.java
@@ -0,0 +1,13 @@
+package risetech.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagement99ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
